e3f53a6b45b131238b795d50c8f82bc3a28ab8cf
Cavell Blood <cavellblood@gmail.com>
2022-07-06 11:33:29 -0500

    Update hydrogen config

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -1,18 +1,23 @@
-import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+import {
+  defineConfig,
+  CookieSessionStorage,
+  PerformanceMetricsServerAnalyticsConnector,
+} from '@shopify/hydrogen/config';
 
 export default defineConfig({
-  shopify: {
-    defaultCountryCode: 'US',
+  shopify: () => ({
     defaultLanguageCode: 'EN',
+    defaultCountryCode: 'US',
     storeDomain: 'farmers-friend.myshopify.com',
     storefrontToken: '71c2b3d50ebc314c389a6979c7bdaf1c',
     storefrontApiVersion: '2022-07',
-  },
+  }),
   session: CookieSessionStorage('__session', {
     path: '/',
     httpOnly: true,
-    secure: import.meta.env.PROD,
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'Strict',
     maxAge: 60 * 60 * 24 * 30,
   }),
+  serverAnalyticsConnectors: [PerformanceMetricsServerAnalyticsConnector],
 });
