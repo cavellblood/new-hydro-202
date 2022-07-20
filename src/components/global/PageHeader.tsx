3895eb480e75c34a7dff2fbb146d80c46b826363
Cavell Blood <cavellblood@gmail.com>
2022-07-20 10:36:55 -0500

    Remove `inline-block` class from heading

--- src/components/global/PageHeader.tsx
+++ src/components/global/PageHeader.tsx
@@ -27,7 +27,7 @@ export function PageHeader({
   return (
     <header {...props} className={styles}>
       {heading && (
-        <Heading as="h1" width="narrow" size="heading" className="inline-block">
+        <Heading as="h1" width="narrow" size="heading">
           {heading}
         </Heading>
       )}
