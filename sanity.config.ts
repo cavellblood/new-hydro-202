f7357638d54da3ed356d33e721a571a14461cffd
Cavell Blood <cavellblood@gmail.com>
2022-07-27 11:37:11 -0500

    Another try

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,6 @@
 export default {
   apiVersion: 'v2022-07-01',
-  dataset:
-    // @ts-ignore
-    process.env.SANITY_DATASET ||
-    import.meta.env.VITE_SANITY_STUDIO_API_DATASET,
+  dataset: import.meta.env.VITE_SANITY_STUDIO_API_DATASET || 'production',
   projectId: '0yukpfvl',
   useCdn: true,
 };
