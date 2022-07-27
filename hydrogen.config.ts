6d342d09286dbdac4591b3588a9c4a0bc3320117
Cavell Blood <cavellblood@gmail.com>
2022-07-27 08:58:05 -0500

    Update domain and token for Oxygen environment

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -8,14 +8,19 @@ export default defineConfig({
   shopify: () => ({
     defaultLanguageCode: 'EN',
     defaultCountryCode: 'US',
-    storeDomain: 'farmers-friend.myshopify.com',
-    storefrontToken: 'e5672a0423776bab229465d75c6868fb',
+    storeDomain:
+      // @ts-ignore
+      Oxygen?.env?.SHOPIFY_STORE_DOMAIN || 'farmers-friend.myshopify.com',
+    storefrontToken:
+      // @ts-ignore
+      Oxygen?.env?.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN ||
+      'f1899f8942bd91fb56122245dd603914',
     storefrontApiVersion: '2022-07',
   }),
   session: CookieSessionStorage('__session', {
     path: '/',
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: import.meta.env.PROD,
     sameSite: 'Strict',
     maxAge: 60 * 60 * 24 * 30,
   }),
