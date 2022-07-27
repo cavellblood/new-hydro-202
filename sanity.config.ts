c2c5b7b2571d9c17482b8766b713aba784ac1c9c
Cavell Blood <cavellblood@gmail.com>
2022-07-27 11:17:57 -0500

    Use `process.env.SANITY_DATASET` to set the Sanity dataset

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,9 @@
 export default {
   apiVersion: 'v2022-07-01',
-  dataset: 'production',
+  dataset:
+    // @ts-ignore
+    process.env.SANITY_DATASET ||
+    import.meta.env.VITE_SANITY_STUDIO_API_DATASET,
   projectId: '0yukpfvl',
   useCdn: true,
 };
