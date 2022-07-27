d1524bfef44f80f356b7bb449d8de84b9358e91a
Cavell Blood <cavellblood@gmail.com>
2022-07-27 10:51:32 -0500

    Update sanity config to use Oxygen variables

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,9 @@
 export default {
   apiVersion: 'v2022-07-01',
-  dataset: import.meta.env.VITE_SANITY_STUDIO_API_DATASET,
+  dataset:
+    // @ts-ignore
+    Oxygen?.env?.SANITY_STUDIO_API_DATASET ||
+    import.meta.env.VITE_SANITY_STUDIO_API_DATASET,
   projectId: '0yukpfvl',
   useCdn: true,
 };
